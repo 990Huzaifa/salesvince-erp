2d5abcf9c3169ef84c82487c0773794ebdd1ba77
syed-huzaifa-ali <syedhuzaifaali366@gmail.com>
2026-06-03 15:36:26 +0500

    minor

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'THIS IS Salesvince ERP';
+    return 'THIS IS Salesvince ERP from the platform backend';
   }
 }
